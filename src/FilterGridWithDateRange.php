750a341b155b894044eab2d3eaa4e2491738fd15
Manish Kumar <manishkumar@deepspace9.tech>
2024-11-28 17:05:18 +0530

    default daterange filter timezone issue fixed

--- src/FilterGridWithDateRange.php
+++ src/FilterGridWithDateRange.php
@@ -27,12 +27,12 @@ class FilterGridWithDateRange
         if (!empty($appliedFilters)) {
             $relationAndColumnName = explode('.', $appliedFilters[0]);
             if (is_numeric($appliedFilters[1])) {
-                $startDate = Carbon::now('Asia/Kolkata')->subMonths($appliedFilters[1])->startOfDay()->toDateTimeString();
-                $endDate = Carbon::now('Asia/Kolkata')->endOfDay()->toDateTimeString();
+                $startDate = Carbon::now('Asia/Kolkata')->subMonths($appliedFilters[1])->startOfDay()->setTimezone('UTC')->toDateTimeString();
+                $endDate = Carbon::now('Asia/Kolkata')->endOfDay()->setTimezone('UTC')->toDateTimeString();
             } else {
                 $dateRange = explode(" to ", $appliedFilters[1]);
-                $startDate = Carbon::parse($dateRange[0], 'Asia/Kolkata')->startOfDay()->toDateTimeString();
-                $endDate = Carbon::parse($dateRange[1], 'Asia/Kolkata')->endOfDay()->toDateTimeString();
+                $startDate = Carbon::parse($dateRange[0], 'Asia/Kolkata')->startOfDay()->setTimezone('UTC')->toDateTimeString();
+                $endDate = Carbon::parse($dateRange[1], 'Asia/Kolkata')->endOfDay()->setTimezone('UTC')->toDateTimeString();
             }
             $this->grid
                 ->getConfig()
